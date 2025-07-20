8686640435f5e3a65321bd4620b8cedab9b0b556
Erdenezul Batmunkh <erdenezulb@gmail.com>
2025-07-20 08:39:31 +0200

    🧑‍💻 Fix prisma autocomplete

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ResourceModule } from './resource/resource.module';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ResourceModule, UserModule],
+  imports: [ResourceModule, UserModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
