c75c8a8fe328e9cb286d59e0e7bc3c351c41ec51
Erdenezul Batmunkh <erdenezulb@gmail.com>
2025-07-20 08:00:41 +0200

    📝 Add explanation for user resources endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,6 +11,13 @@ export class UserController {
   @Get('user/:id/resources')
   /**
    * Return all resources that belong to user
+   * @keep_in_mind
+   * 1. Overlapping shares( direct + via group)
+   * 2. Global shares
+   * @behavior
+   * 1. When user had a direct access to resource, then it should return as part of list
+   * 2. When user had a direct access to resource and also group access, then it should only return
+   *    one entity as part of list
    */
   async userResources(@Param('id') userId: string): Promise<UserResource[]> {
     this.logger.log('[User resources:]', { userId });
